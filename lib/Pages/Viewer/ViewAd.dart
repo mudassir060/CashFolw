2fa2388393633d4bf32e10c6b5f2d4a9ffa66dbe
mudassir060 <mudassirmukhtar4@gmail.com>
2022-01-10 23:12:22 +0500

    Ad View

--- lib/Pages/Viewer/ViewAd.dart
+++ lib/Pages/Viewer/ViewAd.dart
@@ -1,23 +1,49 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
 class ViewAd extends StatefulWidget {
+  const ViewAd({Key? key}) : super(key: key);
+
   @override
-  createState() => _ViewAdState();
+  _ViewAdState createState() => _ViewAdState();
 }
+
 class _ViewAdState extends State<ViewAd> {
   final _key = UniqueKey();
+  // var runing_time = 0;
+  // run() {
+  //   for (var i = 0; i < 30; i++) {
+  //    sleep(const Duration(seconds:1));
+  //     setState(() {
+  //       runing_time++;
+  //     });
+  //     print("============>>>>>$runing_time");
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: Column(
+    // run();
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
           children: [
             Expanded(
-                child: WebView(
-                    key: _key,
-                    javascriptMode: JavascriptMode.unrestricted,
-                    initialUrl: "www.google.com"))
+              child: WebView(
+                key: _key,
+                initialUrl: "https://www.youtube.com/watch?v=TKFDm4m3O6c",
+                javascriptMode: JavascriptMode.unrestricted,
+              ),
+            ),
+            const Center(
+                child: CircularProgressIndicator(
+              strokeWidth: 6,
+            )),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ViewAd();
+    return const ViewAd();
     const Dashboard(
       Name: "Name",
       Email: "Email",
